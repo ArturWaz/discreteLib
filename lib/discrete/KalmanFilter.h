15ad4995146a00e8d9bc0831660bc17fd060cc76
artur <arturwaz11@gmail.com>
2015-04-22 16:48:18 +0200

    up

---
+++ lib/discrete/KalmanFilter.h
@@ -0,0 +1,25 @@
+//
+// Created by szymek on 22/04/15.
+//
+
+#ifndef DISCRETELIB_KALMANFILTER_H
+#define DISCRETELIB_KALMANFILTER_H
+
+
+
+
+
+namespace geox {
+
+    class KalmanFilter {
+
+
+
+    public:
+
+
+    };
+
+}
+
+#endif //DISCRETELIB_KALMANFILTER_H
